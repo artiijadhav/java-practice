538c8b45bea87845e50d30446989a16e1f0da3dd
Arti Jadhav <jadhavarti242@gmail.com>
2026-08-19 20:48:04 +0530

    Add remove vowels program

---
+++ RemoveVowels.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RemoveVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = Character.toLowerCase(str.charAt(i));
+
+            if (ch != 'a' && ch != 'e' && ch != 'i' &&
+                ch != 'o' && ch != 'u') {
+                result += str.charAt(i);
+            }
+        }
+
+        System.out.println("String without vowels: " + result);
+
+        sc.close();
+    }
+}
