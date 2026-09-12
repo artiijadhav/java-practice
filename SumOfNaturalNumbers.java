18b6d8660e1655a9a03d2783269156c64c5145b0
Arti Jadhav <jadhavarti242@gmail.com>
2026-09-12 19:27:57 +0530

    Add sum of natural numbers program

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the limit: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println("Sum of natural numbers = " + sum);
+
+        sc.close();
+    }
+}
