5c9e5f979f8aad54b061e8e0076be4e62a94b74d
Arti Jadhav <jadhavarti242@gmail.com>
2026-09-20 10:37:37 +0530

    Add perfect square program

---
+++ PerfectSquare.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PerfectSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        boolean isPerfectSquare = false;
+
+        for (int i = 1; i * i <= num; i++) {
+            if (i * i == num) {
+                isPerfectSquare = true;
+                break;
+            }
+        }
+
+        if (isPerfectSquare) {
+            System.out.println(num + " is a Perfect Square.");
+        } else {
+            System.out.println(num + " is not a Perfect Square.");
+        }
+
+        sc.close();
+    }
+}
